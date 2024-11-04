553fe94b49005eead8d3f671be9fe9d85d24991c
Pulcin <157858035+Pulcin@users.noreply.github.com>
2024-11-04 07:58:43 -0300

    Add files via upload

---
+++ Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    'Jogos',
+    'Quem e o jogador top 1 do rank global no clash royale',
+    'Mohamed light'
+)
+
+criaCartao(
+    'Carros',
+    'Nome da empresa de carros eletricos mais famosa do mundo',
+    'Tesla'
+)
+
+criaCartao(
+    'Jogos',
+    'Nome do personagem principal de God of War',
+    'Kratos'
+)
+
+criaCartao(
+    'Geografia',
+    'Ponto mais alto no Brasil',
+    'Pico da neblina'
+)
+
+criaCartao(
+    'Enem',
+    'Qual o tema da redaçao de 2024',
+    'Desafios para a valorizaçao da heranca africana no Brasil'
+)
+criaCartao(
+    'Neymar',
+    'Nome da ex do neymar',
+    'Bruna Marquezine'
+)
+criaCartao(
+    'CR7',
+    'Nome da mulher do robozao',
+    'Georgina'
+)
+criaCartao(
+    'Messi',
+    'Quantas bolas de ouro tem o Messi',
+    '8'
+)
+criaCartao(
+    'Vini jr',
+    'Quantas bola de ouro Vini jr ganhou',
+    '0'
+)
